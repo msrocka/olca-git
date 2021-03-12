5c95e606977acb826bd62964b85f2dc13e50bd35
msrocka <michael.srocka@gmail.com>
2021-03-12 17:14:53 +0100

    write objects

--- src/main/java/org/openlca/git/RepoWriter.java
+++ src/main/java/org/openlca/git/RepoWriter.java
@@ -2,12 +2,13 @@ package org.openlca.git;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 import com.google.protobuf.Message;
 import com.google.protobuf.util.JsonFormat;
 
 import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.CommitBuilder;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.FileMode;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectInserter;
@@ -17,11 +18,41 @@ import org.eclipse.jgit.lib.TreeFormatter;
 import org.openlca.core.database.Derby;
 import org.openlca.core.database.IDatabase;
 import org.openlca.core.model.Actor;
+import org.openlca.core.model.Currency;
+import org.openlca.core.model.DQSystem;
+import org.openlca.core.model.Flow;
+import org.openlca.core.model.FlowProperty;
+import org.openlca.core.model.ImpactCategory;
+import org.openlca.core.model.ImpactMethod;
+import org.openlca.core.model.Location;
 import org.openlca.core.model.ModelType;
+import org.openlca.core.model.Parameter;
+import org.openlca.core.model.Process;
+import org.openlca.core.model.ProductSystem;
+import org.openlca.core.model.Project;
+import org.openlca.core.model.SocialIndicator;
+import org.openlca.core.model.Source;
+import org.openlca.core.model.UnitGroup;
+import org.openlca.core.model.Version;
 import org.openlca.core.model.descriptors.Descriptor;
 import org.openlca.git.DbTree.Node;
 import org.openlca.proto.output.ActorWriter;
+import org.openlca.proto.output.CurrencyWriter;
+import org.openlca.proto.output.DQSystemWriter;
+import org.openlca.proto.output.FlowPropertyWriter;
+import org.openlca.proto.output.FlowWriter;
+import org.openlca.proto.output.ImpactCategoryWriter;
+import org.openlca.proto.output.ImpactMethodWriter;
+import org.openlca.proto.output.LocationWriter;
+import org.openlca.proto.output.ParameterWriter;
+import org.openlca.proto.output.ProcessWriter;
+import org.openlca.proto.output.ProductSystemWriter;
+import org.openlca.proto.output.ProjectWriter;
+import org.openlca.proto.output.SocialIndicatorWriter;
+import org.openlca.proto.output.SourceWriter;
+import org.openlca.proto.output.UnitGroupWriter;
 import org.openlca.proto.output.WriterConfig;
+import org.slf4j.LoggerFactory;
 
 public class RepoWriter {
 
@@ -30,9 +61,9 @@ public class RepoWriter {
   private final PersonIdent committer;
 
   public RepoWriter(
-      IDatabase db,
-      Repository repo,
-      PersonIdent committer) {
+    IDatabase db,
+    Repository repo,
+    PersonIdent committer) {
     this.repo = repo;
     this.db = db;
     this.committer = committer;
@@ -45,6 +76,8 @@ public class RepoWriter {
     System.out.printf("loaded DB tree in %.3f sec%n", time / 1000d);
 
     try (var inserter = repo.newObjectInserter()) {
+
+      // build the tree
       var root = new TreeFormatter();
       for (var type : ModelType.values()) {
         var node = dbTree.getRoot(type);
@@ -56,6 +89,25 @@ public class RepoWriter {
         }
       }
 
+      // create the commit
+      var treeID = root.insertTo(inserter);
+      var commit = new CommitBuilder();
+      commit.setAuthor(committer);
+      commit.setCommitter(committer);
+      commit.setMessage("first commit");
+      commit.setEncoding(StandardCharsets.UTF_8);
+      commit.setTreeId(treeID);
+      var commitID = inserter.insert(commit);
+
+      // update the head
+      var ref = repo.findRef("HEAD");
+      var update = repo.updateRef(ref.getName());
+      update.setNewObjectId(commitID);
+      update.update();
+
+    } catch (Exception e) {
+      var log = LoggerFactory.getLogger(getClass());
+      log.error("failed to sync tree", e);
     }
   }
 
@@ -75,6 +127,21 @@ public class RepoWriter {
       var data = serialize(d);
       if (data == null)
         continue;
+      try {
+        var blobID = inserter.insert(Constants.OBJ_BLOB, data);
+        var name = d.refId + "_" + Version.asString(d.version);
+        tree.append(name, FileMode.REGULAR_FILE, blobID);
+      } catch (Exception e) {
+        var log = LoggerFactory.getLogger(getClass());
+        log.error("failed to insert blob for " + d, e);
+      }
+    }
+    try {
+      return tree.insertTo(inserter);
+    } catch (Exception e) {
+      var log = LoggerFactory.getLogger(getClass());
+      log.error("failed to insert tree", e);
+      return null;
     }
   }
 
@@ -84,9 +151,53 @@ public class RepoWriter {
       return null;
 
     Message message = null;
+    var config = WriterConfig.of(db);
+
     if (model instanceof Actor) {
       var w = new ActorWriter(WriterConfig.of(db));
       message = w.write((Actor) model);
+    } else if (model instanceof Currency) {
+      var w = new CurrencyWriter(config);
+      message = w.write((Currency) model);
+    } else if (model instanceof DQSystem) {
+      var w = new DQSystemWriter(config);
+      message = w.write((DQSystem) model);
+    } else if (model instanceof Flow) {
+      var w = new FlowWriter(config);
+      message = w.write((Flow) model);
+    } else if (model instanceof FlowProperty) {
+      var w = new FlowPropertyWriter(config);
+      message = w.write((FlowProperty) model);
+    } else if (model instanceof ImpactCategory) {
+      var w = new ImpactCategoryWriter(config);
+      message = w.write((ImpactCategory) model);
+    } else if (model instanceof ImpactMethod) {
+      var w = new ImpactMethodWriter(config);
+      message = w.write((ImpactMethod) model);
+    } else if (model instanceof Location) {
+      var w = new LocationWriter(config);
+      message = w.write((Location) model);
+    } else if (model instanceof Parameter) {
+      var w = new ParameterWriter(config);
+      message = w.write((Parameter) model);
+    } else if (model instanceof Process) {
+      var w = new ProcessWriter(config);
+      message = w.write((Process) model);
+    } else if (model instanceof ProductSystem) {
+      var w = new ProductSystemWriter(config);
+      message = w.write((ProductSystem) model);
+    } else if (model instanceof Project) {
+      var w = new ProjectWriter(config);
+      message = w.write((Project) model);
+    } else if (model instanceof SocialIndicator) {
+      var w = new SocialIndicatorWriter(config);
+      message = w.write((SocialIndicator) model);
+    } else if (model instanceof Source) {
+      var w = new SourceWriter(config);
+      message = w.write((Source) model);
+    } else if (model instanceof UnitGroup) {
+      var w = new UnitGroupWriter(config);
+      message = w.write((UnitGroup) model);
     }
 
     if (message == null)
@@ -95,6 +206,8 @@ public class RepoWriter {
       var json = JsonFormat.printer().print(message);
       return json.getBytes(StandardCharsets.UTF_8);
     } catch (Exception e) {
+      var log = LoggerFactory.getLogger(getClass());
+      log.error("failed to serialize " + d, e);
       return null;
     }
   }
@@ -105,7 +218,7 @@ public class RepoWriter {
     var repoDir = new File("target/testrepo/.git");
 
     try (var db = Derby.fromDataDir("ei22");
-        var repo = new FileRepository(repoDir)) {
+         var repo = new FileRepository(repoDir)) {
       if (!repoDir.exists()) {
         repo.create(true); // bare repo
       }
